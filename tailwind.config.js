1dddb479d4e42ac11e72f12ec9e512188b04aace
Chlna6666 <79357769+Chlna6666@users.noreply.github.com>
2025-11-22 22:48:21 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx,html}",
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx,html}"
   ],
   theme: {
-    extend: {
-      animation: {
-        progressIndeterminate: "progressIndeterminate 1.2s linear infinite",
-      },
-      keyframes: {
-        progressIndeterminate: {
-          "0%": { transform: "translateX(-150%)" },
-          "100%": { transform: "translateX(300%)" },
-        },
-      },
-      // 如需自定义颜色/字体在这里扩展
-    },
+    extend: {},
   },
   plugins: [],
 };
